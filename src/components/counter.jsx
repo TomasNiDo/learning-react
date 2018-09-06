85d48f0d1053f1743d8c887294998b7bbe9ec7cd
Thomas <thomasg@fligno.com.ph>
2018-09-06 09:13:38 +0800

    Contain buttons to rows

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -7,17 +7,19 @@ class Counter extends Component {
 
   render() {
     return (
-      <div>
-        <span className={ this.getBadgeClasses() }>{ this.formatCount() }</span>
-        <button onClick={ () => this.props.onIncrement(this.props.counter) } className="btn btn-secondary btn-sm">+</button>
-        <button onClick={ () => this.props.onDecrement(this.props.counter) } className="btn btn-secondary btn-sm m-2">-</button>
-        <button onClick={ () => this.props.onDelete(this.props.counter.id) } className="btn btn-danger btn-sm m-2">Delete</button>
+      <div className="row">
+        <div className="col-1">
+          <span className={ this.getBadgeClasses() }>{ this.formatCount() }</span>
+        </div>
+        <button onClick={ () => this.props.onIncrement(this.props.counter) } className="btn btn-secondary btn-sm col-1 m-2">+</button>
+        <button onClick={ () => this.props.onDecrement(this.props.counter) } className="btn btn-secondary btn-sm col-1 m-2">-</button>
+        <button onClick={ () => this.props.onDelete(this.props.counter.id) } className="btn btn-danger btn-sm col-1 m-2">Delete</button>
       </div>
     )
   }
 
   getBadgeClasses() {
-    let classes = "badge m-2 badge-"
+    let classes = "m-2 p-2 badge badge-"
     classes += this.props.counter.value === 0 ? "warning" : "primary"
     return classes
   }
